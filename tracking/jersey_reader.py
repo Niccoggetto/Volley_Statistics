824a11a399253367e646ad6f7ef010b1ae0ff820
Niccoggetto <129863214+Niccoggetto@users.noreply.github.com>
2026-03-11 08:56:46 +0100

    Implement jersey number extraction and OCR methods
    Added methods for extracting, preprocessing, and reading jersey numbers from image crops.

--- tracking/jersey_reader.py
+++ tracking/jersey_reader.py
@@ -21,4 +21,59 @@ class JerseyReader:
 
     def extract_jersey_crop(self, frame, bbox, crop_ratio = 0.5):
         # extract a crop of the jersey area from the bounding box, we will focus on the upper part of the bounding box where the jersey number is usually located
+        # calculate height
+        x1, y1, x2, y2 = bbox
+        height = y2 - y1
+        # get only upper section of the bounding box
+        crop_y2 = y1 + int(height*crop_ratio)
+        crop = frame[y1:crop_y2, x1:x2] # actual crop of the image
+        if crop.size == 0:
+            return None
+        return crop
+    
+
+    def preprocess_crop(self, crop):
+        # improve crop to facilitate OCR readings
+        crop = cv2.resize(crop, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+        # conversion to a scaleof greys
+        gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+        # increase contrast
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        gray = clahe.apply(gray)
+
+        binary = cv2.adaptiveThreshold(
+            gray, 255
+            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            cv2.THRESH_BINARY_INV,
+            11, 2
+        )
+        return binary
+    
+
+    def read_number(self, crop):
+        # perform OCR on the crop and returns found number or Non if no number was found
+        result = self.ocr.ocr(crop, cls=False)
+
+        if not result or not result[0]:
+            return None
+        
+        # get all the found text
+        for line in result[0]:
+            text = line[1][0].strip()
+            confidence = line[1][1]
+        # keep only text that could be jersey numbers
+        if confidence > 0.7 and text.isdigit() and 1 <= int(text)<=99:
+            return int(text)
+        
+        return None
+    
+
+    def update(self, frame, detections):
+        # for each detection try to read the number
+        # majority vote is used to confirm it
+
+        for det in detections:
+            track_id = det["id"]
+            if track_id < 0:
+                continue
         
